1026eaed5ae0427fb93eac53705484124925acf3
Björn Roberg <bjorn.roberg@gmail.com>
2013-11-21 16:59:20 +0100

    updated gitignore and added som init code

--- src/grott/core.clj
+++ src/grott/core.clj
@@ -1,6 +1,31 @@
-(ns grott.core)
+(ns grott.core
+  (:require [lanterna.screen :as s]))
 
-(defn -main
-  [& args]
-  (let [args (set args)]
-    ))
+
+(defrecord Game [world uis input debug-flags])
+
+(defn run-game [game screen]
+  ;(loop [{:keys [input uis] :as game} game])
+  )
+
+(defn new-game []
+  (map->Game {:world nil
+              :uis nil
+              :input nil
+              :debug-flags {:show-regions false}}))
+
+(defn main
+  ([] (main :swing))
+  ([screen-type]
+     (letfn [(go []
+               (let [screen (s/get-screen screen-type)]
+                 (s/in-screen screen
+                              (run-game (new-game) screen))))])))
+
+(defn -main [& args]
+  (let [args (set args)
+        screen-type (cond
+                     (args ":swing") :swing
+                     (args ":text")  :text
+                     :else           :auto)]
+    (main screen-type)))
